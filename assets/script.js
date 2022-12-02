f94d92744697724b4c6ea8b6282684711c9c0439
Isaac Cha <ijcha93@gmail.com>
2022-12-01 20:03:22 -0500

    add nav bar

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,8 @@
 // API KEY `c0e0bc244d6e2e1a2584fe5f12098d0b`
 // "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+var searchBtn = document.querySelector("#clickBtn")
+
+function consoleLog(){
+    searchBtn.onclick(console.log(`I was clicked`))
+}
